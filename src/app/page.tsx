f9e71edc3dff86c4fabd83d0c37282adef93376d
saybz <shailash.bhati@gmail.com>
2023-10-27 23:51:11 +0200

    INIT: intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className="font-head">Shailash Bhati</h1>
+    <main className={"text-white bg-dark " + styles.main}>
+      <section className="flex items-center w-full intro">
+        <div className="w-1/2 left">
+          <div className="flex items-center mb-2 font-head">
+            <span className="text-big text-primary">Hey</span>
+            <h1 className="ml-4 text-xl">
+              {" "}
+              My name is <br /> Shailash Bhati
+            </h1>
+          </div>
+          <div className="max-w-md desc">
+            I’m actually 25 years old and i’m a french web developer based at
+            Tours in France.
+          </div>
+        </div>
+        <div className="flex-col items-center w-1/2 right">
+          <h2 className="mb-2 text-xxl font-head">Say hello</h2>
+          <div>
+            <p>shailash.bhati@gmail.com</p>
+            <p>instagram</p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
         head: ["var(--font-head)"],
         body: ["var(--font-body)"],
       },
+      colors: {
+        primary: "#E6A631",
+        secondary: "#E6A631",
+        dark: "#1E2225",
+      },
+      maxHeight: {
+        desktop: "1440px",
+      },
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      xxl: "1.563rem",
+      "3xl": "2rem",
+      big: "4rem",
     },
   },
   plugins: [],
