63ebc5efc15d752ae999b648ded80634e497c865
saybz <shailash.bhati@gmail.com>
2023-11-02 00:41:34 +0100

    FIX: intro align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={"text-white bg-dark " + styles.main}>
       <section className="flex items-center w-full intro">
-        <div className="w-1/2 left">
+        <div className="w-1/2 pl-12 left">
           <div className="flex items-center mb-2 font-head">
             <span className="text-big text-primary">Hey</span>
             <h1 className="ml-4 text-xl">
@@ -21,7 +21,7 @@ export default function Home() {
             Tours in France.
           </div>
         </div>
-        <div className="flex-col items-center w-1/2 right">
+        <div className="flex-col items-center w-1/2 pl-12 right">
           <h2 className="mb-2 text-xxl font-head">Say hello</h2>
           <div>
             <p>shailash.bhati@gmail.com</p>
